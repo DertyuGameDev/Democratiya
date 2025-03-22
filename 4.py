273b4d76379afeb2f43f69ebb5976c6b23538948
Dertyu2008 <sidovidchenko@gmail.com>
2025-03-22 19:34:38 +0300

    4v2

--- 4.py
+++ 4.py
@@ -1,6 +1,6 @@
 import math
 import sys
-from io import BytesIO  # Этот класс поможет нам сделать картинку из потока байт
+from io import BytesIO
 import requests
 from PIL import Image
 from module import spn1
@@ -12,7 +12,7 @@ def lonlat_distance(a, b):
     b_lon, b_lat = b
 
     # Берем среднюю по широте точку и считаем коэффициент для нее.
-    radians_lattitude = math.radians((a_lat + b_lat) / 2.)
+    radians_lattitude = math.radians((a_lat + b_lat) / 2.0)
     lat_lon_factor = math.cos(radians_lattitude)
 
     # Вычисляем смещения в метрах по вертикали и горизонтали.
@@ -27,7 +27,6 @@ def lonlat_distance(a, b):
 
 # Пусть наше приложение предполагает запуск:
 # python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
 toponym_to_find = " ".join(sys.argv[1:])
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
@@ -35,24 +34,18 @@ geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 geocoder_params = {
     "apikey": "7baececd-be0e-4475-a6ae-f15bef0b9622",
     "geocode": toponym_to_find,
-    "format": "json"}
+    "format": "json"
+}
 
 response = requests.get(geocoder_api_server, params=geocoder_params)
 
 if not response:
-    # обработка ошибочной ситуации
     pass
 
-# Преобразуем ответ в json-объект
 json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
 toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
 toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-org_point = ','.join(
-    json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]['Point']['pos'].split())
 
 search_api_server1 = "https://search-maps.yandex.ru/v1/"
 api_key1 = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
@@ -69,18 +62,23 @@ search_params = {
 
 response = requests.get(search_api_server1, params=search_params)
 if not response:
-    # ...
     pass
 
 json_response1 = response.json()
-# Получаем первую найденную организацию.
-organization = json_response1["features"][:]
-# Адрес организации.
+
+organization = json_response1["features"][:10]
+
+min_lon = min(org["geometry"]["coordinates"][0] for org in organization)
+max_lon = max(org["geometry"]["coordinates"][0] for org in organization)
+min_lat = min(org["geometry"]["coordinates"][1] for org in organization)
+max_lat = max(org["geometry"]["coordinates"][1] for org in organization)
+
+spn_lon = abs(max_lon - min_lon)
+spn_lat = abs(max_lat - min_lat)
+
 org_points = list(map(lambda x: (f"{x[0][0]},{x[0][1]}", x[1]),
                       [(i["geometry"]["coordinates"], i["properties"]["CompanyMetaData"]['Hours']['text'])
                        for i in organization]))
-
-apikey = "ef67d706-4387-4517-8b08-50f4c0929dd7"
 
 
 def metki(org_points):
@@ -95,11 +93,10 @@ def metki(org_points):
     return '~'.join(t)
 
 
-# Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": spn1(json_response),
-    "apikey": apikey,
+    "spn": f"{spn_lon},{spn_lat}",
+    "apikey": "ef67d706-4387-4517-8b08-50f4c0929dd7",
     "pt": metki(org_points)
 }
 
@@ -108,4 +105,4 @@ map_api_server = "https://static-maps.yandex.ru/v1"
 response = requests.get(map_api_server, params=map_params)
 im = BytesIO(response.content)
 opened_image = Image.open(im)
-opened_image.show()  # Создадим картинку и тут же ее покажем встроенным просмотрщиком операционной системы
+opened_image.show()
