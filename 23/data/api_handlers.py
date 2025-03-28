9752c35854beea2368e1b23b83993fd1cf7c174e
Dertyu2008 <sidovidchenko@gmail.com>
2025-03-28 13:52:58 +0300

    23

---
+++ 23/data/api_handlers.py
@@ -0,0 +1,28 @@
+import flask
+from flask import jsonify, make_response
+from data.job import Jobs
+from data import db_session
+
+blueprint = flask.Blueprint('api_handlers', __name__, template_folder='templates')
+
+
+@blueprint.route('/api/jobs')
+def api_jobs():
+    db_sess = db_session.create_session()
+    jobs_data = db_sess.query(Jobs).all()
+
+    for i in range(len(jobs_data)):
+        jobs_data[i] = jobs_data[i].to_dict(
+            only=("id", "team_leader", "job", "work_size", "collaborators", "start_date", "end_date", "is_finished"))
+
+    return jsonify(jobs_data)
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def api_one_jobs(job_id):
+    db_sess = db_session.create_session()
+    jobs_data = db_sess.query(Jobs).get(job_id)
+    if not jobs_data:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify({'jobs': jobs_data.to_dict(
+        only=("id", "team_leader", "job", "work_size", "collaborators", "start_date", "end_date", "is_finished"))})
